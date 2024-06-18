409ad2468d8a2194c35b022f3a61a7a3bf20d4ea
NarawitC <narawit.developer@gmail.com>
2024-06-18 21:55:34 +0700

    feat: ConfigModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,18 @@ import { Module } from '@nestjs/common'
 import { AppController } from './app.controller'
 import { AppService } from './app.service'
 import { UserModule } from './feature/user/user.module'
+import { ConfigModule } from '@nestjs/config'
+import configuration from '@/config/configuration'
 
 @Module({
-  imports: [UserModule],
+  imports: [
+    UserModule,
+    ConfigModule.forRoot({
+      load: [configuration],
+      isGlobal: true,
+      cache: true,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ src/config/configuration.ts
@@ -0,0 +1,11 @@
+export default () => ({
+  port: parseInt(process.env.PORT) || 8000,
+  database: {
+    postgres: {
+      host: process.env.DATABASE_POSTGRES_HOST,
+      port: parseInt(process.env.DATABASE_POSTGRES_PORT) || 5432,
+      userName: process.env.DATABASE_POSTGRES_USERNAME,
+      password: process.env.DATABASE_POSTGRES_PASSWORD,
+    },
+  },
+})
